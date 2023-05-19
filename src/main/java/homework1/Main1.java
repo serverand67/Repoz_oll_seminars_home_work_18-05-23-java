cf107d8cb1199fd68a9a959ce8b06843d1505a79
Veronika040182 <veronika040182@hotmail.com>
2023-05-19 13:34:53 +0300

    Задача 1

---
+++ src/main/java/homework1/Main1.java
@@ -0,0 +1,9 @@
+package homework1;
+import homework1.Task1Thiangle;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Task1Thiangle task1 = new Task1Thiangle();
+        task1.triangleNumber(0);
+    }
+}
--- src/main/java/homework1/Task1Thiangle.java
+++ src/main/java/homework1/Task1Thiangle.java
@@ -1,2 +1,18 @@
-package homework1;public class Task1Thiangle {
+       // Домашнее задание к семинару 1 от 18.05.2023
+       //1. Вычислить n-ое треугольного число(сумма чисел от 1
+       // до n)
+
+package homework1;
+import java.util.Scanner;
+public class Task1Thiangle {
+    public void triangleNumber(int number){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число ");
+        int n = scanner.nextInt();
+//        int n = 5;
+        int result;
+        result = n * (n+1) / 2;
+        System.out.println("Триугольное число, числа " + n + " являестся " + result);
+    }
 }
+
